c0414e435784842776c3571737ba3f4ff1d30c62
milohill <116620023+grimhilt7@users.noreply.github.com>
2023-02-24 09:41:40 +0900

    Finish encodeInCaesarCipher function

--- funcs.js
+++ funcs.js
@@ -7,3 +7,48 @@ export function reverseString(str) {
   if (!str.length || typeof str !== 'string') return;
   return str.split('').reverse().join('');
 }
+
+export const calculator = (() => {
+  const add = (a, b) => a + b;
+  const subtract = (a, b) => a - b;
+  const multiply = (a, b) => a * b;
+  const divide = (a, b) => a / b;
+  return {
+    add,
+    subtract,
+    multiply,
+    divide,
+  };
+})();
+
+function convertToUnicode(str) {
+  return str.split('').map((el) => el.charCodeAt(0));
+}
+
+function shiftUnicode(arr, shiftNumber) {
+  const n = shiftNumber % 26; // sanitize
+  return arr.map((unicode) => {
+    const num = unicode + n;
+    if (unicode >= 97 && unicode <= 122) {
+      // lower case letters
+      return num > 122 ? num - 26 : num;
+    }
+    if (unicode >= 65 && unicode <= 90) {
+      // upper case letters
+      return num > 90 ? num - 26 : num;
+    }
+    return unicode; // skip otherwise such as white space, period, comma, etc
+  });
+}
+
+function convertToString(arr) {
+  return arr.map((el) => String.fromCharCode(el)).join('');
+}
+
+export function encodeInCaesarCipher(str, shiftNumber) {
+  const unicodeArr = convertToUnicode(str);
+  const shiftedArr = shiftUnicode(unicodeArr, shiftNumber);
+  const convertedStr = convertToString(shiftedArr);
+
+  return convertedStr;
+}
--- samples.test.js
+++ samples.test.js
@@ -1,4 +1,9 @@
-import { capitalize, reverseString } from './funcs';
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  encodeInCaesarCipher,
+} from './funcs';
 
 // A capitalize function that takes a string and returns it with the first character capitalized.
 test('capitalize function', () => {
@@ -8,4 +13,24 @@ test('capitalize function', () => {
 // A reverseString function that takes a string and returns it reversed.
 test('reverseString function', () => {
   expect(reverseString('hello')).toBe('olleh');
+});
+
+// A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply.
+test('calculator object', () => {
+  expect(calculator.add(1, 2)).toBe(3);
+  expect(calculator.subtract(1, 2)).toBe(-1);
+  expect(calculator.multiply(1, 2)).toBe(2);
+  expect(calculator.divide(2, 2)).toBe(1);
+});
+
+// A caesarCipher function that takes a string and a shift factor and returns it with each character “shifted”.
+test('caesarCipher function', () => {
+  expect(
+    encodeInCaesarCipher(
+      'If he had anything confidential to say, he wrote it in cipher, that is, by so changing the order of the letters of the alphabet, that not a word could be made out.',
+      7
+    )
+  ).toBe(
+    'Pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol hswohila, aoha uva h dvyk jvbsk il thkl vba.'
+  );
 });
